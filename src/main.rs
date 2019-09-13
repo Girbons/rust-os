a96e7ed527b995d0362cfe746bc32f79ede088aa
Alessandro De Angelis <alessandrodea22@gmail.com>
2019-09-13 10:41:16 +0200

    02

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    loop {}
+}
+
+// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
